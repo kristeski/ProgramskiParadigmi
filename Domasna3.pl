f44050f560830526ee0db13588ef6e7c22c142bb
kristeski <kiko.rici.2007@gmail.com>
2025-12-09 20:58:47 +0100

    domasna 3

--- Domasna3.pl
+++ Domasna3.pl
@@ -1,81 +1,56 @@
-ime(teo).   ime(mira).  ime(bruno).  ime(igor).
-hrana(sendvic).
-hrana(pita).
-hrana(hamburger).
-hrana(pica).
-hobi(krstozbori).
-hobi(pishuvanje).
-hobi(citanje).
-hobi(fotografija).
-maica(bela).
-maica(zolta).
-maica(sina).
-maica(crvena).
-dev(mira). 
-fakt1_pos(teo, 1).
-fakt1_hrana(teo, sendvic).
-fakt2_hobi(mira, krstozbori).
-fakt2_hrana(mira, pita).
-fakt3_maica(bela).
-fakt4_maica(bruno, zolta).
-fakt5_relation(pishuvanje, hamburger).
-fakt6_pokrajteo(pita).
-fakt7_sosed_bruno(pica).
-fakt8_bela_sosed_pica(bela).
-fakt9_hobi(igor, citanje).
-fakt10_desno_sina(sina).
-sosed(1,2).
-sosed(2,1).
-sosed(2,3).
-sosed(3,2).
-sosed(3,4).
-sosed(4,3).
-all_diff([]).
-all_diff([H|T]) :- + member(H,T), all_diff(T).
+lichnost(teo).
+lichnost(mira).
+lichnost(bruno).
+lichnost(igor).
+
+:- dynamic hrana/2. % za da moze da dodavame fakti
+hrana(mira, pita).
+hrana(teo, sendvich).
+
+:-dynamic maica/2.
+maica(bruno, zholta).
+maica(devojka, bela).
+
+:- dynamic mesto/2. % za da moze da dodavame fakti
+mesto(teo, 1).
+
+hobi(mira, krstozbor).
+hobi(igor, cita).
+
+devojka(mira).
+hobi_vo_hrana(cita, hamburger).
+
+:- dynamic smesten/1.
+smesten(teo). %teo e najlevo
+
+%koj nosi maica vo boja B
+boja_maica(Lichnost, B):-
+    maica(Lichnost, B), lichnost(Lichnost), !;
+    devojka(Lichnost), maica(devojka, B), !, assertz(maica(Lichnost, bela)).
+
+%lichnosta sto saka nekoe hobi koe povlekuva hrana
+hrana_hobi() :-
+    hobi_vo_hrana(Hobi, Hrana), lichnost(L), hobi(L, Hobi), !, assertz(hrana(L, Hrana)).
+
+%lichnosta sto jade pita sedi pokraj teo
+lichnosta_do_teo() :-
+    lichnost(X), hrana(X, pita), mesto(teo, Y1), Y is Y1 + 1, !,assertz(mesto(X, Y)), !, assertz(smesten(X)).
+
+%Bruno sedi pokraj onoj sto saka pica
+smesten(bruno).
+bruno_do_pica():-
+    lichnost(L), hrana(L, pica), mesto(L, Y1), Y is Y1 + 1, !, assertz(mesto(bruno, Y)).
+
+%lichnosta sto sedi pokraj onaa so bela maica saka pica
+lichnosta_do_devojceto() :-
+    lichnost(L), \+smesten(L), boja_maica(L1, bela), mesto(L1, Y1), Y is Y1 + 1, !, assertz(hrana(L, pica)), !, assertz(mesto(L, Y)).
+
+%sina maica ima licnosta koja sedi desno od devojkata
+desno_dev():-
+    devojka(X), mesto(X, Y), Y1 is Y + 1, mesto(L, Y1), !, assertz(maica(L, sina)).
+
+getans(L, Hr, Ho, Ma, Me):-
+    hrana(L, Hr), hobi(L, Ho), maica(L, Ma), mesto(L, Me).
+
 reshenie(L) :-
-    PosT = 1,
-    member(PosM, [2,3,4]),
-    member(PosB, [2,3,4]),
-    member(PosI, [2,3,4]),
-    all_diff([PosT,PosM,PosB,PosI]),
-    HrT = sendvic,
-    HrM = pita,
-    hrana(HrB),
-    hrana(HrI),
-    HobM = krstozbori,
-    HobI = citanje,
-    hobi(HobT),
-    hobi(HobB),
-    MaiM = bela,
-    MaiB = zolta,
-    maica(MaiT),
-    maica(MaiI),
-    (HobT = pishuvanje -> HrT = hamburger ; true),
-    (HobB = pishuvanje -> HrB = hamburger ; true),
-    (HobI = pishuvanje -> HrI = hamburger ; true),
-    (HobM = pishuvanje -> HrM = hamburger ; true),
-    sosed(PosT, PosM),
-    (HrT = pica -> sosed(PosB, PosT) ; true),
-    (HrM = pica -> sosed(PosB, PosM) ; true),
-    (HrI = pica -> sosed(PosB, PosI) ; true),
-    sosed(PosM, PosP),
-    (
-        (PosP = PosT, HrT = pica);
-        (PosP = PosB, HrB = pica);
-        (PosP = PosI, HrI = pica)
-    ),
-    PosSina is PosM + 1,
-    (
-        (PosSina = PosT, MaiT = sina);
-        (PosSina = PosB, MaiB = sina);
-        (PosSina = PosI, MaiI = sina)
-    ),
-    all_diff([HrT,HrM,HrB,HrI]),
-    all_diff([HobT,HobM,HobB,HobI]),
-    all_diff([MaiT,MaiM,MaiB,MaiI]),
-    L = [
-        (teo,  HrT, HobT, MaiT),
-        (mira, HrM, HobM, MaiM),
-        (igor, HrI, HobI, MaiI),
-        (bruno,HrB, HobB, MaiB)
-    ].
+    lichnost(X), getans(X, Hr, Ho, Ma, Me), L = [L|[X, Hr, Ho, Ma, Me]].
